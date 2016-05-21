5c2f4306d0402334c2f3a40ce5e5d421624017de
Michael Busta <busta2356@gmail.com>
2016-05-20 20:57:58 -0400

    fixing stripe api call 2

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,6 +1,13 @@
 class ChargesController < ApplicationController
   require "stripe"
-  Stripe.api_key = ENV['stripe_key']
+
+  Rails.configuration.stripe = {
+  :publishable_key => ENV['PUBLISHABLE_KEY'],
+  :secret_key      => ENV['SECRET_KEY']
+}
+
+Stripe.api_key = Rails.configuration.stripe[:secret_key]
+
 
   def new
     @package = Package.find(params[:package])
